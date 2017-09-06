94f9ac26f3d52b9b3f240ed6aec3c14b959ecc62
root <q351477@163.com>
2017-09-06 16:26:14 +0800

    set tabwidget second page English translate

--- customtab.cpp
+++ customtab.cpp
@@ -11,15 +11,29 @@ CustomTab::CustomTab(QWidget *parent) :
     ui->tabWidget->setTabText(page0, tr("Basic settings"));
     ui->tabWidget->setTabText(page1, tr("Network"));
     ui->tabWidget->setTabText(page2, tr("About"));
+    ui->tabWidget->tabBar()->setStyle(new CustomTabStyle);
 
     ui->lab_note->setText(tr("note"));
     ui->lab_serverAddr->setText(tr("server address"));
     ui->lab_port->setText(tr("port"));
     ui->cco_auto->setText(tr("Auto adjust resolution"));
     ui->cco_manual->setText(tr("Manual adjust resolution"));
-    ui->btn_save->setText(tr("Save"));
+    ui->btn_basic_save->setText(tr("Save"));
 
-    ui->tabWidget->tabBar()->setStyle(new CustomTabStyle);
+    ui->rbn_dhcp->setText(tr("Automatic access using DHCP"));
+    ui->rbn_staticIP->setText(tr("Using a static IP address"));
+    ui->lab_ipAddr->setText(tr("IP address"));
+    ui->lab_subnetMask->setText(tr("Subnet mask"));
+    ui->lab_gateway->setText(tr("Default gateway"));
+
+    ui->rbn_autoDNS->setText(tr("Automatically get the DNS server address"));
+    ui->rbn_manualDNS->setText(tr("Manually set the DNS server adress"));
+
+    ui->lab_dnsAddr1->setText(tr("The first DNS server"));
+    ui->lab_dnsAddr2->setText(tr("The second DNS server"));
+    ui->lab_dnsAddr3->setText(tr("The third DNS server "));
+    ui->btn_network_save->setText(tr("Save"));
+
 }
 
 CustomTab::~CustomTab()
